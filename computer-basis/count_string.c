83a671a61ae4dd2097210c3292d3ea8b7ba7225c
hsh-me@outlook.com <hsh-me@outlook.com>
2019-07-17 12:35:41 +0800

    test

---
+++ computer-basis/count_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    long int c;
+    a = 18;
+    b = 'B';
+    c = 1234567889;
+    printf("请输入: %d , %ld", a, c);
+    getchar();
+    getchar();
+    
+    return 0;
+}
