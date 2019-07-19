912894a1ea2370d3f87a9adbd946a128d4a24553
hsh-me@outlook.com <hsh-me@outlook.com>
2019-07-19 08:37:41 +0800

    字符串长度计算

--- computer-basis/count_string.c
+++ computer-basis/count_string.c
@@ -1,14 +1,30 @@
+/*
+ * @Author: huangsh
+ * @Date: 2019-07-17 13:22:09
+ * @Description: 计算字符串长度。
+ */
+
 #include <stdio.h>
+
+int count_str(char str[])
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
 
 int main()
 {
-    int a;
-    long int c;
-    a = 18;
-    c = 1234567889;
-    printf("请输入: %d , %ld", a, c);
-    getchar();
-    getchar();
-    
-    return 0;
+    char str[50];
+    int count;
+
+    printf("请输入一个字符串：");
+    gets(str);
+
+    count = count_str(str);
+    printf("字符长度为：%d", count);
+    return 0;    
 }
